4cd4130ae8c4c7a78d2950e1275d17e012fec4c8
jshmsyc <nrzxsyc@live.com>
2023-10-10 18:56:10 +0800

    :sparkles: update about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,10 +1,59 @@
-import React from 'react'
-import './About.css'
+import React from "react";
+import "./About.css";
 export default function About() {
   return (
     <div>
-      <h1>People</h1>
-      <h1>Publication</h1>
+      <div className="people-wrapper">
+        <h2>People</h2>
+        <div className="card-item">
+          <div className="card-img yc"></div>
+          <h4>Yuancheng Shen</h4>
+          <p>Shandong University</p>
+        </div>
+        <div className="card-item">
+          <div className="card-img zy"></div>
+          <h4>Yue Zhao</h4>
+          <p>Shandong University</p>
+        </div>
+        <div className="card-item">
+          <div className="card-img gt"></div>
+          <h4>Tong Ge</h4>
+          <p>Shandong University</p>
+        </div>
+        <div className="card-item">
+          <div className="card-img shy"></div>
+          <h4>Haoyan Shi</h4>
+          <p>Shandong University</p>
+        </div>
+        <div className="card-item">
+          <div className="card-img bl"></div>
+          <h4>Bongshin Lee</h4>
+          <p>Microsoft Research</p>
+        </div>
+        <div className="card-item">
+          <div className="card-img yhw"></div>
+          <h4>Yunhai Wang</h4>
+          <p>Shandong University</p>
+        </div>
+      </div>
+      <div className="pub-wrapper">
+        <h2>Publication</h2>
+        <div className="pub-item">
+          <div className="pub-info">
+            <h4>
+              Authoring Data-Driven Chart Animations[
+              <a href="./CASTPlus.pdf" target="_blank">pdf</a>]
+            </h4>
+            <p>
+              Yuancheng Shen, Yue Zhao, Tong Ge, Haoyan Shi, Bongshin Lee,
+              Yunhai Wang
+            </p>
+            <p>
+              <i></i>
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
